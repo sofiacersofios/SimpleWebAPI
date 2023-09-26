8726ce58a1894af134cd22fcfbc7eccb38ad9444
ingsoft3ucc <140459109+ingsoft3ucc@users.noreply.github.com>
2023-09-26 15:29:46 -0300

    Update WeatherForecastController.cs

--- SimpleWebAPI/Controllers/WeatherForecastController.cs
+++ SimpleWebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,8 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Helado", "Bracing", "Fresquito", "Fresco", "Mild", "Calido", "Balmy", "Caloron", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
